771f63e1afa63feedd5e53f9729818e3200b657f
Leonard0nt <leopoldo.wall@inacapmail.cl>
2023-11-28 19:34:23 -0300

    prueba para levantar

--- sistema_disponibilidad_bomberos_parral/settings.py
+++ sistema_disponibilidad_bomberos_parral/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
-import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +83,7 @@ WSGI_APPLICATION = 'sistema_disponibilidad_bomberos_parral.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 #activar mysql
 import pymysql
